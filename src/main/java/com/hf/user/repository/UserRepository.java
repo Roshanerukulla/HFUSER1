0e54f3d7fdc18c8762d2cb7921603a64d788ab8f
Roshanerukulla <125940568+Roshanerukulla@users.noreply.github.com>
2024-03-18 00:55:44 -0400

    Update UserRepository.java

--- src/main/java/com/hf/user/repository/UserRepository.java
+++ src/main/java/com/hf/user/repository/UserRepository.java
@@ -1,9 +1,16 @@
 package com.hf.user.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.hf.user.entity.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
+
+    @Query("SELECT u.username FROM User u WHERE u.id IN :ids")
+    List<String> findUsernamesByIds(@Param("ids") List<Long> ids);
 }
