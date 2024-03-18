f0e46221fdeec852f8c8343fc28421bcc0df0770
Roshanerukulla <125940568+Roshanerukulla@users.noreply.github.com>
2024-03-18 00:38:06 -0400

    Update RegistrationController.java

--- src/main/java/com/hf/user/controller/RegistrationController.java
+++ src/main/java/com/hf/user/controller/RegistrationController.java
@@ -1,8 +1,5 @@
 package com.hf.user.controller;
 
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
